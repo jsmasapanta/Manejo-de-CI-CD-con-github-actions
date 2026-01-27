656070658b7bbd8f18eb7e970236e2e4ae6c55af
jsmasapanta <jsmasapanta@espe.edu.ec>
2026-01-27 12:39:33 -0500

    Corregir pruebas y lint para CI

--- sum.test.js
+++ sum.test.js
@@ -1,9 +1,9 @@
-const sum= required ('./sum');
+import sum from './sum.js';
 
 test('suma 3 + 4 debe ser 7', () => {
-    expect(sum(3,4)).toBe(7);
+  expect(sum(3, 4)).toBe(7);
 });
 
 test('suma -1 + 1 debe ser 0', () => {
-    expect(sum(-1,1)).toBe(0);
+  expect(sum(-1, 1)).toBe(0);
 });
